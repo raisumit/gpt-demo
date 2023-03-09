c680fd2c3fbd3748867a2c49f71c5457ff7c197c
sumit <raisumit986@gmail.com>
2023-03-09 17:59:15 +0530

    updated the server hostaed url from render.com

--- client/script.js
+++ client/script.js
@@ -85,8 +85,8 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-
-    const response = await fetch('http://localhost:5000', {
+//http://localhost:5000
+    const response = await fetch('https://fine-tuned-idfc-modal.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
